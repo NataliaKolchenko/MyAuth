00ac1d5dfedce17a5b104798ea333102896cf3c4
Natalia <kolchenko.nata.yu@gmail.com>
2024-09-13 11:51:38 +0200

    created AuthController

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.AppUser;
+import com.example.demo.model.dto.*;
+import com.example.demo.service.jwt.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AppUser> register(@RequestBody RegisterRequestDto registerRequestDto){
+        return ResponseEntity.ok(authService.register(registerRequestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto){
+        return ResponseEntity.ok(authService.login(loginRequestDto));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<RefreshTokenResponseDto> refresh(@RequestBody RefreshTokenRequestDto refreshTokenRequestDto) {
+        return ResponseEntity.ok(authService.refresh(refreshTokenRequestDto));
+    }
+}
