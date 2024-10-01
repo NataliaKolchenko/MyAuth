513822b85e45ee3a843dc61b85f816ddffaaa2df
Natalia <kolchenko.nata.yu@gmail.com>
2024-10-01 17:35:28 +0200

    this project is working

--- src/main/java/com/example/demo/service/jwt/JwtSecurityService.java
+++ src/main/java/com/example/demo/service/jwt/JwtSecurityService.java
@@ -1,11 +1,9 @@
 package com.example.demo.service.jwt;
 
-import com.example.demo.repository.AppUserRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -15,28 +13,24 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Service
-@RequiredArgsConstructor
 public class JwtSecurityService {
     private static final String SECRET_KEY = "5Hdo5+PxMJkLQ9Wo7WnYMR/gBzTfC5XrB3iNPvMlscY=";
-    private final AppUserService appUserService;
 
-    private SecretKey getSigningKey() {
+    private SecretKey getSigningKey(){
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public String generateToken(UserDetails userDetails) {
+    public String generateToken(UserDetails userDetails){
         return Jwts.builder()
                 .subject(userDetails.getUsername())
-                .claim("role", appUserService.getUserInfo(userDetails).getRole())
-                .claim("userId", appUserService.getUserInfo(userDetails).getId())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSigningKey())
                 .compact();
     }
 
-    public String generateRefreshToken(Map<String, String> claims, UserDetails userDetails) {
+    public String generateRefreshToken(Map<String, String> claims, UserDetails userDetails){
         return Jwts.builder()
                 .claims(claims)
                 .subject(userDetails.getUsername())
@@ -46,9 +40,9 @@ public class JwtSecurityService {
                 .compact();
     }
 
-    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    private <T> T extractClaim (String token, Function<Claims, T> claimsResolver){
         final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
+        return  claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
@@ -60,48 +54,29 @@ public class JwtSecurityService {
     }
 
     // Получение имени юзера (он же почта)
-    public String extractUsername(String token) {
+    public String extractUsername (String token){
         return extractClaim(token, Claims::getSubject);
     }
 
     // Когда срок действия заканчивается
-    public Date extractExpiration(String token) {
+    public Date extractExpiration (String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
     // Когда выдан токен
-    public Date extractIssuedAt(String token) {
+    public Date extractIssuedAt (String token){
         return extractClaim(token, Claims::getIssuedAt);
     }
 
     // Метод проверки срока действия токена
-    public boolean isTokenExpired(String token) {
+    public boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
     }
 
     // Метод для валидации токена
-    public boolean validateToken(String token, UserDetails userDetails) {
+    public boolean validateToken (String token, UserDetails userDetails){
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())
                 && !isTokenExpired(token));
-    }
-
-    //проверяет валидность токена, полученного из заголовка запроса.
-    public boolean validateTokenFromHeader(String authHeader) {
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            return false;
-        }
-
-        String token = authHeader.substring(7);
-
-        try {
-            String username = extractUsername(token);
-            UserDetails userDetails = appUserService
-                    .getDetailsService()
-                    .loadUserByUsername(username);
-            return validateToken(token, userDetails);
-        } catch (Exception e) {
-            return false;
-        }
     }
 }
