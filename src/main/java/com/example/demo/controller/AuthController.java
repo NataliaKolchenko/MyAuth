1a6d81c548bfe1e044e59d88bd428cf7b8357945
Natalia <kolchenko.nata.yu@gmail.com>
2024-11-28 14:00:35 +0100

    add swagger

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.model.dto.*;
 import com.example.demo.service.jwt.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authorisation Controller", description = "Authorisation Controller allows to users make Authorisation and get JWT")
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(summary = "Registration of user in Authorisation Service")
     @PostMapping("/register")
     public ResponseEntity<RegisterResponseDto> register(@RequestBody @Valid RegisterRequestDto registerRequestDto){
         return ResponseEntity.ok(authService.register(registerRequestDto));
     }
 
+    @Operation(summary = "Login of user in Authorisation Service")
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(  @Valid @RequestBody LoginRequestDto loginRequestDto){
         return ResponseEntity.ok(authService.login(loginRequestDto));
